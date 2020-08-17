1dd4d5863c3bbe0798068aa471a46de4b954fffa
billlanyon <billlanyon@me.com>
2020-08-17 20:39:54 +0200

    #10: Adds authetication middleware.

--- graphql/resolvers/booking.js
+++ graphql/resolvers/booking.js
@@ -3,7 +3,10 @@ const Booking = require('../../models/booking');
 const { transformBooking, transformEvent } = require('./merge');
 
 module.exports = {
-    bookings: async () => {
+    bookings: async (args, req) => {
+        if (!req.isAuth) {
+            throw new Error('Unauthenticated.');
+        }
         try {
             const bookings = await Booking.find();
             return bookings.map(booking => {
@@ -13,7 +16,10 @@ module.exports = {
             throw err;
         }
     },
-    bookEvent: async args => {
+    bookEvent: async (args, req) => {
+        if (!req.isAuth) {
+            throw new Error('Unauthenticated.');
+        }
         const fetchedEvent = await Event.findOne({ _id: args.eventId });
         const booking = new Booking({
             user: '5f363a8616d8653de446df9e',
@@ -22,7 +28,10 @@ module.exports = {
         const result = await booking.save();
         return transformBooking(result);
     },
-    cancelBooking: async args => {
+    cancelBooking: async (args, req) => {
+        if (!req.isAuth) {
+            throw new Error('Unauthenticated.');
+        }
         try {
             const booking = await Booking.findById(args.bookingId).populate('event');
             const event = transformEvent(booking.event);
--- graphql/resolvers/events.js
+++ graphql/resolvers/events.js
@@ -1,10 +1,12 @@
 const Event = require('../../models/event');
+const User = require('../../models/user');
+
 const { transformEvent } = require('./merge');
 
 module.exports = {
     events: async () => {
         try {
-            const events = await Event.find()
+            const events = await Event.find();
             return events.map(event => {
                 return transformEvent(event);
             });
@@ -12,19 +14,22 @@ module.exports = {
             throw err;
         }
     },
-    createEvent: async args => {
+    createEvent: async (args, req) => {
+        if (!req.isAuth) {
+            throw new Error('Unauthenticated.');
+        }
         const event = new Event({
             title: args.eventInput.title,
             description: args.eventInput.description,
             price: +args.eventInput.price,
-            date: dateToString(args.eventInput.date),
-            creator: '5f363a8616d8653de446df9e'
+            date: new Date(args.eventInput.date),
+            creator: req.userId
         });
         let createdEvent;
         try {
             const result = await event.save();
             createdEvent = transformEvent(result);
-            const creator = await User.findById('5f363a8616d8653de446df9e');
+            const creator = await User.findById(req.userId);
 
             if (!creator) {
                 throw new Error('User not found.');
